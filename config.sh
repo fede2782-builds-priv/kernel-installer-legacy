ff5d38e15a6afd6f4e4f6340b4b61eaa08979ca0
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-09-16 16:03:57 +0200

    Add basic lagacy modules support

--- config.sh
+++ config.sh
@@ -1,6 +1,10 @@
 # Config file for kernel installer
 SYSTEM_BLOCK=/dev/block/by-name/system
 BOOT_BLOCK=/dev/block/by-name/boot
+VENDOR_BLOCK=/dev/block/by-name/vendor
 DEVICE_CODENAME="river"
 DEVICE_NAME="Moto G7"
 IS_SLOT=true
+IS_MODULES=true
+MODULE_PARTITION="vendor"
+MODULES_DIR="lib"
